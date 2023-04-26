8f4e62ff07a792525ecc96dfb092414c71df57ea
Nishan Budhathoki <nishanbudhathoki2266@gmail.com>
2023-04-26 16:03:04 +0545

    Installed mongoose and configured database credentials

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-// For knowing about environment variables
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+const app = require('./app');
+
+// For knowing about environment variables
 
 // for knowing the directory of our environment variables
 dotenv.config({ path: './config.env' })
 
-const app = require('./app');
+const DB = process.env.DATABASE.replace('<PASSOWRD>', process.env.DATABASE_PASSWORD);
+
+console.log(DB);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
