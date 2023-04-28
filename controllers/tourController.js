0a1e2dc11737d0a19ebbe137361e52b2abcfb442
Nishan Budhathoki <nishanbudhathoki2266@gmail.com>
2023-04-28 15:16:04 +0545

    Added a aggregation pipeline for tour stats and monthly plan

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -170,3 +170,102 @@ exports.deleteTour = async (req, res) => {
         )
     }
 }
+
+
+// Aggregation pipeline 
+exports.getTourStats = async (req, res, next) => {
+    try {
+        const stats = await Tour.aggregate([
+            {
+                $match: { ratingsAverage: { $gte: 4.5 } }
+            },
+            {
+                $group: {
+                    _id: { $toUpper: '$difficulty' },
+                    numTours: { $sum: 1 },
+                    numRatings: { $sum: '$ratingsQuantity' },
+                    avgRating: { $avg: '$ratingsAverage' },
+                    avgPrice: { $avg: '$price' },
+                    minPrice: { $min: '$price' },
+                    maxPrice: { $max: '$price' },
+                }
+            },
+            {
+                $sort: {
+                    avgPrice: 1
+                }
+            }
+        ])
+
+        res.status(200).json({
+            stauts: 'success',
+            data: {
+                stats
+            }
+        })
+    }
+    catch (err) {
+        res.status(404).json(
+            {
+                stauts: 'fail',
+                message: err
+            }
+        )
+    }
+}
+
+exports.getMonthlyPlan = async (req, res) => {
+    try {
+        const year = +req.params.year;
+
+        const plan = await Tour.aggregate([
+            {
+                $unwind: '$startDates'
+            },
+            {
+                $match: {
+                    startDates: {
+                        $gte: new Date(`${year}-01-01`),
+                        $lte: new Date(`${year}-12-31`),
+                    }
+                }
+            },
+            {
+                $group: {
+                    _id: { $month: '$startDates' },
+                    numTourStarts: { $sum: 1 },
+                    tours: { $push: '$name' } // Adds a tour name in the array to display for a month
+                }
+            },
+            {
+                $addFields: { month: '$_id' }
+            },
+            {
+                $project: {
+                    _id: 0
+                }
+            },
+            {
+                $sort: { numTourStarts: -1 }
+            },
+            // {
+            //     $limit: 3 // To get the top 3 busiest month
+            // }
+        ])
+
+        res.status(200).json({
+            stauts: 'success',
+            data: {
+                plan
+            }
+        })
+    }
+    catch (err) {
+        res.status(404).json(
+            {
+                stauts: 'fail',
+                message: err
+            }
+        )
+    }
+}
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,7 +5,10 @@ const router = express.Router();
 
 // Param middleware -> middleware that runs only when a param is present 
 
+
 router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours)
+router.route('/tour-stats').get(tourController.getTourStats)
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan)
 
 router.route('/').get(tourController.getAllTours).post(tourController.createTour);
 
