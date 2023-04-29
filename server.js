0e1d433231c623f0c5f58c9132c62c27eea34d87
Nishan Budhathoki <nishanbudhathoki2266@gmail.com>
2023-04-29 16:34:23 +0545

    Added some uncaught errors as well as operational errors

--- server.js
+++ server.js
@@ -1,34 +1,49 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+process.on("uncaughtException", (err) => {
+  console.log(err.name, err.message);
+  console.log("Unhandled rejection! Shutting down!");
+  //   Code 1 stands for uncaught exception
+  process.exit(1);
+});
 
 // For knowing about environment variables
 // for knowing the directory of our environment variables
-dotenv.config({ path: './config.env' })
+dotenv.config({ path: "./config.env" });
 
-// First you need to read environment files and then only run app 
-const app = require('./app');
+// First you need to read environment files and then only run app
+const app = require("./app");
 
 // Making a db connection string by replacing password template with the real
-const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD.replace('@', '%40'));
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD.replace("@", "%40")
+);
 
 const connectDb = async () => {
-    try {
-        const connection = await mongoose.connect(DB, {
-            autoIndex: true
-        })
-        console.log("Database Connection Successful!");
-    }
-    catch (error) {
-        console.log("Something went wrong while connecting to the database!", err);
-    }
-}
+  try {
+    const connection = await mongoose.connect(DB, {
+      autoIndex: true,
+    });
+    console.log("Database Connection Successful!");
+  } catch (error) {
+    console.log("Something went wrong while connecting to the database!", err);
+  }
+};
 
-connectDb()
-
+connectDb();
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log("App running on port", port);
+const server = app.listen(port, () => {
+  console.log("App running on port", port);
 });
 
-
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
+  console.log("Unhandled rejection! Shutting down!");
+  server.close(() => {
+    //   Code 1 stands for uncaught exception
+    process.exit(1);
+  });
+});
