a01561a10acffc268475748c6b196c1879cb67f9
Nishan Budhathoki <nishanbudhathoki2266@gmail.com>
2023-05-01 13:07:23 +0545

    Added a delete me functionality

--- Models/userModel.js
+++ Models/userModel.js
@@ -42,7 +42,12 @@ const userSchema = new mongoose.Schema({
         type: Date,
     },
     passwordResetToken: String,
-    passwordResetExpires: Date
+    passwordResetExpires: Date,
+    active: {
+        type: Boolean,
+        default: true,
+        select: false
+    }
 });
 
 
@@ -64,6 +69,12 @@ userSchema.pre('save', async function (next) {
 
     next();
 
+})
+
+userSchema.pre(/^find/, function (next) {
+    // this points to the current query 
+    this.find({ active: { $ne: false } });
+    next();
 })
 
 // Instance method
