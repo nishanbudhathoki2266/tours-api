c73634a41704852a1564a3e7f92b345e28492f49
Nishan Budhathoki <nishanbudhathoki2266@gmail.com>
2023-05-01 12:48:20 +0545

    Implemented update me functionality for the user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,16 @@
 const User = require("../Models/userModel");
 const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
+
+const filterObj = (obj, ...allowedFields) => {
+    const newObj = {}
+    Object.keys(obj).forEach(el => {
+        if (allowedFields.includes(el)) {
+            newObj[el] = obj[el];
+        }
+    })
+    return newObj;
+}
 
 // users
 exports.getAllUsers = catchAsync(async (req, res) => {
@@ -13,6 +24,24 @@ exports.getAllUsers = catchAsync(async (req, res) => {
             users,
         },
     });
+})
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+    // Create error if use posts password data
+    if (req.body.password || req.body.passwordConfirm) {
+        return next(new AppError("This route is not for password updates. Please use /updateMyPassword", 400))
+    }
+
+    // Update the user documents
+    const filteredBody = filterObj(req.body, 'name', 'email');
+    const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, { new: true, runValidators: true });
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            user: updatedUser
+        }
+    })
 })
 
 exports.getUser = (req, res) => {
