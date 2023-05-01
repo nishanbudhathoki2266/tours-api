85e03af92035192752e8e8b869907039ef123506
Nishan Budhathoki <nishanbudhathoki2266@gmail.com>
2023-05-01 12:31:19 +0545

    Implemented an update password functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,6 +12,7 @@ const signToken = id => {
     });
 }
 
+
 exports.signup = catchAsync(async (req, res, next) => {
     // Only taking the things that are just required to sign up the user. things like roles and all are neglected as anyone can be an admin while signing up
     const newUser = await User.create({
@@ -167,3 +168,28 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
         token
     })
 })
+
+exports.updatePassword = catchAsync(async (req, res, next) => {
+    //  Get user from the collection
+    const user = await User.findById(req.user.id).select('+password');
+
+    // Check if posted current password is correct
+    if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
+        return next(new AppError("Your current password is wrong.", 401))
+    }
+
+    // If so, update password
+    user.password = req.body.password;
+    user.passwordConfirm = req.body.passwordConfirm;
+    // FindByIdandUpdate wont work as the validator only works in create and save
+    await user.save();
+
+    // Log user in, send JWT
+    const token = signToken(user._id);
+
+    res.status(200).json({
+        status: 'success',
+        token
+    })
+})
+
