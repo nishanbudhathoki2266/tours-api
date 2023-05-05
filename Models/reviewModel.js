658664bbf6a25ce660d330901275c30b755fa6bb
Nishan Budhathoki <nishanbudhathoki2266@gmail.com>
2023-05-05 20:20:56 +0545

    Added number of ratings and rating averages

--- Models/reviewModel.js
+++ Models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = mongoose.Schema({
     review: {
@@ -38,6 +39,47 @@ reviewSchema.pre(/^find/, function (next) {
         path: 'user',
         select: 'name photo'
     });
+    next();
+})
+
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+    const stats = await this.aggregate([
+        {
+            $match: { tour: tourId }
+        },
+        {
+            $group: {
+                _id: '$tour',
+                nRating: { $sum: 1 },
+                avgRating: { $avg: '$rating' }
+            }
+        }
+    ]);
+
+    if (stats.length > 0) {
+        await Tour.findByIdAndUpdate(tourId, { ratingsQuantity: stats[0].nRating, ratingsAverage: stats[0].avgRating })
+    }
+    else {
+        await Tour.findByIdAndUpdate(tourId, { ratingsQuantity: 0, ratingsAverage: 4.5 })
+
+    }
+}
+
+reviewSchema.post('save', function (next) {
+    // this points to the current review where as this.constructor refers to the model
+    this.constructor.calcAverageRatings(this.tour);
+    // next();
+})
+
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+    this.r = await this.findOne();
+    console.log(r);
+    next();
+})
+
+// with this.r we are transfering data from pre to post
+reviewSchema.post(/^findOneAnd/, async function (next) {
+    await this.r.constructor.calcAverageRatings(this.r.tour);
     next();
 })
 
