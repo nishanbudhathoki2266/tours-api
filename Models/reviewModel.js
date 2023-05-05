e2ea63aae748f682661521d8a2b639f53695815e
Nishan Budhathoki <nishanbudhathoki2266@gmail.com>
2023-05-05 20:29:20 +0545

    Prevented adding duplicate reviews

--- Models/reviewModel.js
+++ Models/reviewModel.js
@@ -31,6 +31,9 @@ const reviewSchema = mongoose.Schema({
         toObject: { virtuals: true },
     });
 
+// Preventing duplicate reviews
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true })
+
 reviewSchema.pre(/^find/, function (next) {
     this.populate({
         path: 'tour',
